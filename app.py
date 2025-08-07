3b735e7bdb627d197c7742630616a6986119f88b
DAVIDNONCI <118031572+DAVIDNONCI@users.noreply.github.com>
2025-08-07 20:28:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
 # app.py
 import streamlit as st
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import joblib
-
-# Load data
-iris = load_iris()
-X = iris.data
-y = iris.target
-
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Train model
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-# Save model
-joblib.dump(model, 'iris_model.joblib')
+import numpy as np
 
 # Load model
 model = joblib.load('iris_model.joblib')
@@ -44,7 +26,4 @@ if st.button('Prediksi'):
     st.subheader(f'Hasil Prediksi: {species[prediction[0]]}')
     
     # Tambahkan visualisasi (opsional)
-
     st.bar_chart(model.predict_proba(input_data)[0])
-
-
